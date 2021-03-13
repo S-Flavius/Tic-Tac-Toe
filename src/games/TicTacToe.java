3e3de1c46884984376c2958a5355ceaebf38efaa
Rederdex <flavius.secheli@gmail.com>
2021-03-13 14:01:50 +0100

    .

--- src/games/TicTacToe.java
+++ src/games/TicTacToe.java
@@ -1,30 +1,58 @@
 package games;
 
-@SuppressWarnings ("NestedMethodCall")
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+import java.util.Scanner;
+
+@SuppressWarnings ({"NestedMethodCall", "DuplicateStringLiteralInspection", "MethodCallInLoopCondition", "StatementWithEmptyBody", "ChainedMethodCall", "BooleanVariableAlwaysNegated", "unused", "RedundantFieldInitialization"})
 public class TicTacToe {
 
-    // TODO Draw board
-    // TODO Check game state
-    // TODO Make moves
-    // TODO Random AI
     // TODO good AI
     // TODO GUI
 
     private static final String borders = "---------";
     private static final String emptyLine = "| _ _ _ |";
-    private static final int[] positions = { 0,
-                                             14, 16, 18,
-                                             25, 27, 29,
-                                             36, 38, 40};
+    private static final int[] positions = {0,
+            12, 14, 16,
+            22, 24, 26,
+            32, 34, 36};
+    private static int movecnt = 0;
 
     public static void main(String[] args) {
-        System.out.println(drawBoard());
-        int[] xS = {1,3,5};
-        int[] oS = {4,6,8};
-        System.out.println(drawBoard(xS,oS));
+        StringBuilder board = createBoard();
+
+
+        Scanner input = new Scanner(System.in, StandardCharsets.UTF_8);
+        System.out.print("X moves: ");
+
+        while (!finished(board)) {
+//            int position = input.nextInt();
+            boolean xMov = movecnt % 2 == 0;
+
+            if (xMov) {
+                int position = input.nextInt();
+                xMove(board, position);
+            } else {
+//                oMove(board, position);
+                oAi(board);
+            }
+            System.out.println(board);
+
+            if (xMov && !finished(board)) {
+//                System.out.print("O moves: ");
+            } else {
+                System.out.print("X moves: ");
+            }
+
+            movecnt++;
+        }
+
+        input.close();
+        System.out.println(gameState(board));
+
     }
 
-    private static StringBuilder drawBoard( ) {
+    private static StringBuilder createBoard( ) {
         StringBuilder board = new StringBuilder();
         board.append(borders).append("\n");
         board.append((emptyLine + "\n").repeat(3));
@@ -32,20 +60,131 @@ public class TicTacToe {
         return board;
     }
 
-    private static StringBuilder drawBoard(int[] xS, int[] oS) {
+    private static StringBuilder createBoard(int[] xS, int[] oS) {
         StringBuilder board = new StringBuilder();
+        board.append(borders).append("\n");
+        board.append((emptyLine + "\n").repeat(3));
+        board.append(borders);
 
-        //TODO help me plox
-        for (int i: xS) {
-            System.out.println(positions[i]);
-            System.out.println(positions[i]+1);
-            board.replace(positions[i],positions[i]+2,"X");
+        for (int i : xS) {
+            board.replace(positions[i], positions[i] + 1, "X");
         }
-        for (int i: xS) {
-            board.replace(positions[i],positions[i]+2,"O");
+        for (int i : oS) {
+            board.replace(positions[i], positions[i] + 1, "O");
         }
         return board;
     }
 
+    private static boolean xWon(StringBuilder board) {
+        boolean line1, line2, line3, row1, row2, row3, diag1, diag2;
+        line1 = board.substring(positions[1], positions[3] + 1).equals("X X X");
+        line2 = board.substring(positions[4], positions[6] + 1).equals("X X X");
+        line3 = board.substring(positions[7], positions[9] + 1).equals("X X X");
+        row1 = board.substring(positions[1], positions[1] + 1).equals("X") &&
+                board.substring(positions[4], positions[6] + 1).equals("X") &&
+                board.substring(positions[7], positions[9] + 1).equals("X");
+        row2 = board.substring(positions[2], positions[2] + 1).equals("X") &&
+                board.substring(positions[5], positions[5] + 1).equals("X") &&
+                board.substring(positions[8], positions[8] + 1).equals("X");
+        row3 = board.substring(positions[3], positions[3] + 1).equals("X") &&
+                board.substring(positions[6], positions[6] + 1).equals("X") &&
+                board.substring(positions[9], positions[9] + 1).equals("X");
+        diag1 = board.substring(positions[1], positions[1] + 1).equals("X") &&
+                board.substring(positions[5], positions[5] + 1).equals("X") &&
+                board.substring(positions[9], positions[9] + 1).equals("X");
+        diag2 = board.substring(positions[3], positions[3] + 1).equals("X") &&
+                board.substring(positions[5], positions[5] + 1).equals("X") &&
+                board.substring(positions[7], positions[7] + 1).equals("X");
+        return line1 || line2 || line3 || row1 || row2 || row3 || diag1 || diag2;
+    }
+
+    private static boolean oWon(StringBuilder board) {
+        boolean line1, line2, line3, row1, row2, row3, diag1, diag2;
+        line1 = board.substring(positions[1], positions[3] + 1).equals("O O O");
+        line2 = board.substring(positions[4], positions[6] + 1).equals("O O O");
+        line3 = board.substring(positions[7], positions[9] + 1).equals("O O O");
+        row1 = board.substring(positions[1], positions[1] + 1).equals("O") &&
+                board.substring(positions[4], positions[4] + 1).equals("O") &&
+                board.substring(positions[7], positions[7] + 1).equals("O");
+        row2 = board.substring(positions[2], positions[2] + 1).equals("O") &&
+                board.substring(positions[5], positions[5] + 1).equals("O") &&
+                board.substring(positions[8], positions[8] + 1).equals("O");
+        row3 = board.substring(positions[3], positions[3] + 1).equals("O") &&
+                board.substring(positions[6], positions[6] + 1).equals("O") &&
+                board.substring(positions[9], positions[9] + 1).equals("O");
+        diag1 = board.substring(positions[1], positions[1] + 1).equals("O") &&
+                board.substring(positions[5], positions[5] + 1).equals("O") &&
+                board.substring(positions[9], positions[9] + 1).equals("O");
+        diag2 = board.substring(positions[3], positions[3] + 1).equals("O") &&
+                board.substring(positions[5], positions[5] + 1).equals("O") &&
+                board.substring(positions[7], positions[7] + 1).equals("O");
+        return line1 || line2 || line3 || row1 || row2 || row3 || diag1 || diag2;
+    }
+
+    private static boolean finished(StringBuilder board) {
+        boolean x = xWon(board);
+        boolean o = oWon(board);
+        boolean emptySpace = true;
+        for (int i = 1; i < positions.length; i++) {
+            if (board.substring(positions[i], positions[i] + 1).equals("_")) {
+                emptySpace = true;
+                break;
+            } else {
+                emptySpace = false;
+            }
+        }
+        boolean draw = !emptySpace && !x && !o;
+
+        return draw || x || o;
+    }
+
+    private static String gameState(StringBuilder board) {
+        if (finished(board)) {
+            if (xWon(board)) {
+                return "Player X won the game!";
+            } else if (oWon(board)) {
+                return "Player O won the game!";
+            } else {
+                return "Draw!";
+            }
+        } else {
+            return "The game has not yet finished!";
+        }
+    }
+
+    private static void xMove(StringBuilder board, int position) {
+        if (freeSpace(board, position)) {
+            board.replace(positions[position], positions[position] + 1, "X");
+        } else {
+            System.out.println("Spot taken, choose another spot");
+            movecnt--;
+        }
+    }
+
+    private static void oMove(StringBuilder board, int position) {
+        if (freeSpace(board, position)) {
+            board.replace(positions[position], positions[position] + 1, "O");
+        } else {
+            System.out.println("Spot taken, choose another spot");
+            movecnt--;
+        }
+    }
+
+    private static void oAi(StringBuilder board) {
+        Random generator = new Random();
+        int move = generator.nextInt(9) + 1;
+
+        do {
+            if (!freeSpace(board, move)) {
+                move = generator.nextInt(9) + 1;
+            }
+        } while (!freeSpace(board, move));
+        board.replace(positions[move], positions[move] + 1, "O");
+
+    }
+
+    private static boolean freeSpace(StringBuilder board, int position) {
+        return board.substring(positions[position], positions[position] + 1).equals("_");
+    }
 
 }
